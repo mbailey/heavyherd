57eae9882e8d34d9e8651798457274da28996ade
Mike Bailey <mike@bailey.net.au>
2009-03-22 20:31:59 +1100

    More style updates

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,23 @@
+class ArtistsController < ApplicationController
+  # GET /artists
+  # GET /artists.xml
+  def index
+    @artists = Artist.find(:all)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @artists }
+    end
+  end
+
+  # GET /artists/1
+  # GET /artists/1.xml
+  def show
+    @artist = Artist.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @artist }
+    end
+  end
+end
